230a10887b2dff693a40188daa7fb52d452c33ae
Sanjar Suvonov <sanjar.tatu.96@gmail.com>
2021-02-02 23:40:02 +0500

    Dagger Hilt

--- app/src/main/java/uz/pdp/hilt/data/model/User.kt
+++ app/src/main/java/uz/pdp/hilt/data/model/User.kt
@@ -1,7 +1,7 @@
 package uz.pdp.hilt.data.model
 
 data class User(
-    val id: Int = 0,
-    val name: String = "",
-    val email: String = "",
+    val id: Int,
+    val name: String,
+    val email: String
 )
